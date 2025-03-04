4979088610e00198573334364666c0d12d4699b0
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2025-03-04 10:21:05 +0100

    comments

--- src/test/java/com/mytests/spring/springpropertiestestpropertysource/SpringTestPropertySourcesTest.java
+++ src/test/java/com/mytests/spring/springpropertiestestpropertysource/SpringTestPropertySourcesTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 // Spring Debugger:
 // all properties can be evaluated in debugger
 // for the overridden properties the actual value is shown in the corresponding *.properties files
-// (application.properties and test_messages.properties added via @TestPropertySource#locations
+// (application.properties and test_messages.properties added via @TestPropertySource#locations)
 // Problems:
 // Source|Override for the property evaluation results works only for local.prop2 that is set in application.properties only.
 // Other sources (test_messages.properties and @TestPropertySource#properties) are not navigable
@@ -20,13 +20,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class SpringTestPropertySourcesTest {
 
     // set in test_messages.properties, overridden in @TestPropertySource#properties
-    @Value("${tm.prop1}")
+    @Value("${tm.prop1}") // no folded value is displayed
     String attr0;
     // set in test_messages.properties only
-    @Value("${tm.prop2}")
+    @Value("${tm.prop2}") // no folded value is displayed
     String attr1;
     // set in application.properties, overridden in @TestPropertySource#properties
-    @Value("${local.prop1}")
+    @Value("${local.prop1}")   // folded value displays the one from application.properties instead of the overridden one
     String attr2;
     // set in application.properties only
     @Value("${local.prop2}")
